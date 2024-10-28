56ecc494951d7ebba10f86e9f59e7d385313dfd7
Yanice <git@yanice.me>
2024-10-28 18:25:05 +0100

    Initial Commit

---
+++ Spenit/SpenitApp.swift
@@ -0,0 +1,32 @@
+//
+//  SpenitApp.swift
+//  Spenit
+//
+//  Created by Yan :) on 28/10/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SpenitApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
